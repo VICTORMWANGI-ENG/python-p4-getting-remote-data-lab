9756fb09f3fc60cf75a9860dc37c016034831082
VICTORMWANGI-ENG <mwangizeus69@gmail.com>
2024-10-12 16:23:14 +0300

    feat: implement GetRequester class
    Adds the `GetRequester` class, enabling retrieval and parsing of JSON data from a given URL. The class provides methods for fetching the raw response body and converting it to a Python dictionary. This facilitates the integration of external JSON data sources into the project.

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,21 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
-
+        
+    
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
+        
+            
 
     def load_json(self):
-        pass
+        response = requests.get(self.url)
+        return json.loads(response.content)
+    
+
+get_requester = GetRequester(
+    'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+print(get_requester.load_json())
